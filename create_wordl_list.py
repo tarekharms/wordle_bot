6f0e269ce44e1b869b6f2f9359a09da1c57c16bd
Tarek Harms <tarek.harms@outlook.de>
2022-03-20 12:55:06 +0100

    INIT

---
+++ create_wordl_list.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+print(sys.argv)
+
+if(sys.argv[1] == 'en'):
+    os.system('aspell -l en dump master | aspell -l en expand > words')
+elif(sys.argv[1] == 'de'):
+    os.system('aspell -l de dump master | aspell -l de expand > words')
+else:
+    print("Keine Sprache angegeben")
+    exit()
+    
+
+wordFile = open("words", 'r')
+
+woerterbuch = []
+
+for line in wordFile:
+    line = line.replace("\n", '')
+    words = line.split(' ')
+
+    for word in words:
+        word = word.lower()
+
+        if(len(sys.argv) >= 3 and sys.argv[2] == '--ohne-umlaute'):
+            word = word.replace('ä', 'ae')
+            word = word.replace('ö', 'oe')
+            word = word.replace('ü', 'ue')
+            word = word.replace('ß', 'ss')
+
+        if(len(word) == 5 and '\'' not in word):
+            woerterbuch.append(word)
+
+wordFile.close()
+os.system('rm words')
+
+woerterbuch = list(dict.fromkeys(woerterbuch))
+woerterbuch.sort()
+
+for wort in woerterbuch:
+    print(wort)
+    
+wordleFile = open("wordleWords_" + sys.argv[1], 'w')
+
+for wort in woerterbuch:
+    wordleFile.write(wort + '\n')
+
+wordleFile.close()
---
+++ wordle_en.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+import string
+
+def getPossibleWords(woerterbuch, possibleWordmatch, vorhandeneBuchstaben, woerterBlacklist):
+    words = []
+
+    for wort in woerterbuch:
+        wort = wort.replace('\n', '')
+        
+        if(wort in woerterBlacklist):
+            continue
+
+        if(
+            wort[0] in possibleWordmatch[0] and
+            wort[1] in possibleWordmatch[1] and
+            wort[2] in possibleWordmatch[2] and
+            wort[3] in possibleWordmatch[3] and
+            wort[4] in possibleWordmatch[4] 
+        ):
+            buchstabenVorhanden = True
+
+            for buchstabe in vorhandeneBuchstaben:
+                if(buchstabe not in wort):
+                    buchstabenVorhanden = False
+
+            if(buchstabenVorhanden):
+                words.append(wort)
+
+    return words
+
+def getWordThatHisMostIfAllFalse(possibleWords, richtigeBuchstaben):
+    killerWord = possibleWords[0]
+    killerWordKillCount = 0
+
+    for possibleKiller in possibleWords:
+        killCount = getKillCountOfWord(possibleWords, possibleKiller, richtigeBuchstaben)
+
+        if(killCount > killerWordKillCount):
+            killerWord = possibleKiller
+            killerWordKillCount = killCount
+
+    return killerWord
+
+
+
+def getKillCountOfWord(possibleWords, killerWord, richtigeBuchstaben):
+    killcount = 0
+
+    for word in possibleWords:
+        kills = False
+
+        for i in range(0, 5):
+            if(killerWord[i] in word and not richtigeBuchstaben[i]):
+                killcount += 1
+                kills = True
+                break
+
+        if kills: continue
+
+    return killcount
+
+def hatGewonnen(richtigeBuchstaben):
+    gewonnen = True
+
+    for richtig in richtigeBuchstaben:
+        if(not richtig):
+            gewonnen = False
+
+    return gewonnen
+
+
+alphabet = list(string.ascii_lowercase)
+
+# Darfenthaltensein und muss in einem enthalten sein.
+possibleWordmatch = [
+    list(string.ascii_lowercase), 
+    list(string.ascii_lowercase),
+    list(string.ascii_lowercase),
+    list(string.ascii_lowercase),
+    list(string.ascii_lowercase)
+]
+
+richtigeBuchstaben = [False, False, False, False, False]
+vorhandeneBuchstaben = []
+woerterBlacklist = ['aires', 'aries', 'kiowa']
+
+while(True):    
+    woerterbuch = open("wordleWords_en", 'r')
+    possibleWords = getPossibleWords(woerterbuch, possibleWordmatch, vorhandeneBuchstaben, woerterBlacklist)
+    woerterbuch.close()
+
+    print(possibleWords)
+    print(len(possibleWords))
+
+    zuRatendesWort = getWordThatHisMostIfAllFalse(possibleWords, richtigeBuchstaben)
+    print("Höchste wahrscheinlichkeit: " + zuRatendesWort)
+    muster = input("Ergebnis [R=Richtig, F=Falsch, V=Vorhanden, G=Wort Nicht vorhanden]: ")
+
+    if(muster[0].lower() == 'g'):
+        woerterBlacklist.append(zuRatendesWort)
+        continue
+
+    gerateneswort = zuRatendesWort
+
+    for i in range(0, 5):
+
+        if(muster[i].lower() == 'r'):
+            possibleWordmatch[i] = [gerateneswort[i]]
+
+            if(not richtigeBuchstaben[i] and gerateneswort[i] in vorhandeneBuchstaben):
+                vorhandeneBuchstaben.remove(gerateneswort[i])
+
+            richtigeBuchstaben[i] = True
+
+        elif(muster[i].lower() == 'f'):
+            for match in possibleWordmatch:
+                if(gerateneswort[i] in match):
+                    match.remove(gerateneswort[i])
+
+        elif(muster[i].lower() == 'v'):
+            possibleWordmatch[i].remove(gerateneswort[i])
+
+            if(gerateneswort[i] not in vorhandeneBuchstaben):
+                vorhandeneBuchstaben.append(gerateneswort[i])
+
+
+    if(hatGewonnen(richtigeBuchstaben)):
+        print("Gewonnen")
+        break
+
+
+    
+exit()
+        
